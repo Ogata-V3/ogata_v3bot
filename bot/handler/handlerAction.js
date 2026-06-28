cf270cc42206cba026be1d9a277a3c191f4f50ab
ZORO Vex <mdajmaulislam00@gmail.com>
2026-06-28 16:29:25 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,85 +1,70 @@
 const handlerCheckDB = require("./handlerCheckData.js");
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-	const handlerEventsFile = process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js";
-	const handlerEventsFactory = require(handlerEventsFile);
+	const handlerEventsFactory = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js");
 
 	return async function (event) {
 		try {
-			// Check antiInbox
-			if (
-				global.GoatBot.config.antiInbox == true &&
-				event.isGroup == false &&
-				event.senderID == event.threadID
-			)
-				return;
+		if (
+			global.GoatBot.config.antiInbox == true &&
+			event.isGroup == false &&
+			event.senderID == event.threadID
+		)
+			return;
 
-			// Build message object
-			const messageFunc = global.utils?.message;
-			const message = typeof messageFunc === "function"
-				? messageFunc(api, event)
-				: {
-					reply: (msg) => api.sendMessage(msg, event.threadID),
-					send: (msg) => api.sendMessage(msg, event.threadID),
-					delete: (msgID) => api.unsendMessage(msgID || event.messageID),
-				};
+		const messageFunc = global.utils?.message;
+		const message = typeof messageFunc === "function"
+			? messageFunc(api, event)
+			: {
+				reply: (msg) => api.sendMessage(msg, event.threadID),
+				send: (msg) => api.sendMessage(msg, event.threadID),
+				delete: (msgID) => api.unsendMessage(msgID || event.messageID),
+			};
 
-			// Check DB (don't let this crash the whole flow)
-			try {
-				await handlerCheckDB(usersData, threadsData, event);
-			} catch (dbErr) {
-				const log = global.utils?.log || { err: console.error };
-				log.err("HANDLER_DB", "handlerCheckDB error: " + dbErr.message);
-			}
+		try { await handlerCheckDB(usersData, threadsData, event); }
+		catch (e) { (global.utils?.log || console).err?.("DB", e.message) || console.error("DB", e.message); }
 
-			// Build handler for this event
-			const handlerEvents = handlerEventsFactory(
-				api, threadModel, userModel, dashBoardModel, globalModel,
-				usersData, threadsData, dashBoardData, globalData
-			);
+		const handlerEvents = handlerEventsFactory(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+		const handlerChat = await handlerEvents(event, message);
+		if (!handlerChat)
+			return;
 
-			const handlerChat = await handlerEvents(event, message);
-			if (!handlerChat)
-				return;
+		const {
+			onAnyEvent, onFirstChat, onStart, onChat,
+			onReply, onEvent, handlerEvent, onReaction,
+			typ, presence, read_receipt
+		} = handlerChat;
 
-			const {
-				onAnyEvent, onFirstChat, onStart, onChat,
-				onReply, onEvent, handlerEvent, onReaction,
-				typ, presence, read_receipt
-			} = handlerChat;
 
-			onAnyEvent();
-			switch (event.type) {
-				case "message":
-				case "message_reply":
-				case "message_unsend":
-					onFirstChat();
-					onChat();
-					onStart();
-					onReply();
-					break;
-				case "event":
-					handlerEvent();
-					onEvent();
-					break;
-				case "message_reaction":
-					onReaction();
-					break;
-				case "typ":
-					typ();
-					break;
-				case "presence":
-					presence();
-					break;
-				case "read_receipt":
-					read_receipt();
-					break;
-				default:
-					break;
-			}
-		} catch (err) {
-			const log = global.utils?.log || { err: console.error };
-			log.err("HANDLER", "Unhandled error in handlerAction: " + err.message, err);
+		onAnyEvent();
+		switch (event.type) {
+			case "message":
+			case "message_reply":
+			case "message_unsend":
+				onFirstChat();
+				onChat();
+				onStart();
+				onReply();
+				break;
+			case "event":
+				handlerEvent();
+				onEvent();
+				break;
+			case "message_reaction":
+				onReaction();
+				break;
+			case "typ":
+				typ();
+				break;
+			case "presence":
+				presence();
+				break;
+			case "read_receipt":
+				read_receipt();
+				break;
+			default:
+				break;
 		}
+	} catch(err) { (global.utils?.log || console).err?.("HANDLER", err.message) || console.error("HANDLER", err); }
 	};
 };
