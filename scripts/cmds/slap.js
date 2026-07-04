3eba7e3b1b4e13f452a4f6777b00c7d13f67f565
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-04 12:14:50 +0600

    Update slap.js

--- scripts/cmds/slap.js
+++ scripts/cmds/slap.js
@@ -1,3 +1,7 @@
+const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   config: {
     name: "slap",
@@ -28,19 +32,73 @@ module.exports = {
       const name1 = await usersData.getName(senderID).catch(() => "User");
       const name2 = await usersData.getName(targetID).catch(() => "User");
 
-      const slapImage = "https://i.imgur.com/LyG1bq1.jpeg";
-
       try {
-        const stream = await global.utils.getStreamFromURL(slapImage);
+        // Get profile pictures
+        const senderImg = await loadImage(
+          `https://graph.facebook.com/${senderID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+        ).catch(() => null);
 
-        const replyText = `🤣 ${name1} 𝐬𝐥𝐚𝐩𝐩𝐞𝐝 ${name2}! 😆`;
+        const targetImg = await loadImage(
+          `https://graph.facebook.com/${targetID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+        ).catch(() => null);
 
-        return message.reply({
-          body: replyText,
-          attachment: stream
+        // Load template image
+        const templateImg = await loadImage("https://i.imgur.com/LyG1bq1.jpeg");
+
+        // Create canvas
+        const width = 800;
+        const height = 600;
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext("2d");
+
+        // Draw template background
+        ctx.drawImage(templateImg, 0, 0, width, height);
+
+        // Draw profile pictures as circles with border
+        function drawCircle(ctx, img, x, y, size) {
+          // White border
+          ctx.fillStyle = "#FFFFFF";
+          ctx.beginPath();
+          ctx.arc(x + size / 2, y + size / 2, size / 2 + 5, 0, Math.PI * 2);
+          ctx.fill();
+
+          // Image circle
+          ctx.save();
+          ctx.beginPath();
+          ctx.arc(x + size / 2, y + size / 2, size / 2, 0, Math.PI * 2);
+          ctx.closePath();
+          ctx.clip();
+          if (img) {
+            ctx.drawImage(img, x, y, size, size);
+          }
+          ctx.restore();
+        }
+
+        // Draw slapper and target pictures - positioned better
+        if (senderImg) drawCircle(ctx, senderImg, 80, 140, 220);
+        if (targetImg) drawCircle(ctx, targetImg, 500, 140, 220);
+
+        // Save image
+        const outputPath = path.join(__dirname, "slap_output.png");
+        const stream = canvas.createPNGStream();
+        const out = fs.createWriteStream(outputPath);
+        
+        stream.pipe(out);
+
+        out.on("finish", () => {
+          const replyText = `🤣 ${name1} 𝐬𝐥𝐚𝐩𝐩𝐞𝐝 ${name2}! 😆`;
+
+          message.reply({
+            body: replyText,
+            attachment: fs.createReadStream(outputPath)
+          }, () => {
+            try { fs.unlinkSync(outputPath); } catch (e) {}
+          });
         });
+
       } catch (imageErr) {
-        return message.reply(`🤣 ${name1} 𝐬𝐥𝐚𝐩𝐩𝐞𝐝 ${name2}! 😆\n(𝐂𝐨𝐮𝐥𝐝𝐧'𝐭 𝐥𝐨𝐚𝐝 𝐢𝐦𝐚𝐠𝐞)`);
+        console.error("Image error:", imageErr);
+        return message.reply(`🤣 ${name1} 𝐬𝐥𝐚𝐩𝐩𝐞𝐝 ${name2}! 😆`);
       }
 
     } catch (err) {
