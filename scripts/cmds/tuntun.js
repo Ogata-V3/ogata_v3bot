180f8663ae15e6aef653606a56d986bee8bf04ef
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:03:11 +0600

    Update tuntun.js

--- scripts/cmds/tuntun.js
+++ scripts/cmds/tuntun.js
@@ -11,7 +11,7 @@ module.exports = {
         config: {
                 name: "tuntun",
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 role: 0,
                 category: "fun",
                 cooldown: 10,
