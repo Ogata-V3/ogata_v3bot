a0446dd30bd9c2ff72889a2ddc894f7086b0aefc
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 17:41:13 +0600

    Update pair4.js

--- scripts/cmds/pair4.js
+++ scripts/cmds/pair4.js
@@ -30,21 +30,21 @@ module.exports = {
         langs: {
                 bn: {
                         noGender: "× বেবি, আপনার জেন্ডার প্রোফাইলে সেট করা নেই",
-                        noMatch: "× দুঃখিত, এই গ্রুপে আপনার জন্য কোনো ম্যাচ পাওয়া যায়নি",
+                        noMatch: "× দুঃখিত, এই গ্রুপে আপনার জন্য কোনো ম্যাচ পাওয়া যায়নি",
                         success: "💞 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n• %1\n• %2\n\n𝐋𝐨𝐯𝐞 𝐏𝐞𝐫𝐜𝐞𝐧𝐭𝐚𝐠𝐞: %3%",
-                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact RAHAT।"
+                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact Ajmaul।"
                 },
                 en: {
                         noGender: "× Baby, your gender is not defined in your profile",
                         noMatch: "× Sorry, no match found for you in this group",
                         success: "💞 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n• %1\n• %2\n\n𝐋𝐨𝐯𝐞 𝐏𝐞𝐫𝐜𝐞𝐧𝐭𝐚𝐠𝐞: %3%",
-                        error: "× API error: %1. Contact RAHAT for help."
+                        error: "× API error: %1. Contact Ajmaul for help."
                 },
                 vi: {
                         noGender: "× Cưng ơi, giới tính của cưng không được xác định",
                         noMatch: "× Rất tiếc, không tìm thấy mảnh ghép nào cho cưng",
                         success: "💞 𝐆𝐡𝐞́𝐩 đ𝐨̂𝐢 𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠\n• %1\n• %2\n\n𝐓𝐲̉ 𝐥𝐞̣̂ 𝐭𝐢̀𝐧𝐡 𝐜𝐚̉𝐦: %3%",
-                        error: "× Lỗi: %1. Liên hệ RAHAT để hỗ trợ."
+                        error: "× Lỗi: %1. Liên hệ Ajmaul để hỗ trợ."
                 }
         },
 
