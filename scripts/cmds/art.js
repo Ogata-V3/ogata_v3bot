a8c15dd2ed8513573cc56b107bcceb598f9e86c7
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 14:18:34 +0600

    Update art.js

--- scripts/cmds/art.js
+++ scripts/cmds/art.js
@@ -12,7 +12,7 @@ module.exports = {
                 name: "art",
                 aliases: ["artify", "photoart"],
                 version: "1.7",
-                author: "RAHAT", // credit Change dile thapramu kintu.
+                author: "Ajmaul", // credit Change dile thapramu kintu.
                 countDown: 10,
                 role: 0,
                 description: {
