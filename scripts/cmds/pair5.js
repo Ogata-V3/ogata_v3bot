439e21b36be7e2393a0daee1ff336c5717dac696
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:21:13 +0600

    Update pair5.js

--- scripts/cmds/pair5.js
+++ scripts/cmds/pair5.js
@@ -11,7 +11,7 @@ module.exports = {
         config: {
                 name: "pair5",
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 10,
                 role: 0,
                 description: {
