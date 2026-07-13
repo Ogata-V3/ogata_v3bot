93626357fe483f50272c32c4f8a16c8fcfa4f4d4
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:44:47 +0600

    Update fluxpro.js

--- scripts/cmds/fluxpro.js
+++ scripts/cmds/fluxpro.js
@@ -11,7 +11,7 @@ module.exports = {
         config: {
                 name: "fluxpro",
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 15,
                 role: 0,
                 description: {
