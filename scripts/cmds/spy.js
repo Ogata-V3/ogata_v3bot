d3d39e76d72223d791ee74dd032f1c78cbde69f2
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:09:16 +0600

    Update spy.js

--- scripts/cmds/spy.js
+++ scripts/cmds/spy.js
@@ -10,7 +10,7 @@ module.exports = {
                 name: "spy",
                 aliases: ["spyinfo", "whoami"],
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 10,
                 role: 0,
                 description: {
