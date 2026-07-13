3a1811191c393fd5c1f6ecc8d605b215499ed93f
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:38:16 +0600

    Update hgen.js

--- scripts/cmds/hgen.js
+++ scripts/cmds/hgen.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "hgen",
     version: "1.0",
-    author: "Rifat | nxo_here",
+    author: "Aizen",
     countDown: 5,
     role: 0,
     shortDescription: { en: "Generate NSFW images using prompt" },
