0b02ea4e86d0b4fd991e474981a9600e2950d653
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:35:23 +0600

    Update joke.js

--- scripts/cmds/joke.js
+++ scripts/cmds/joke.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "joke",
     aliases: ["jokes"],
     version: "1.7",
-    author: "RAHAT",
+    author: "Ajmaul",
     countDown: 5,
     role: 0,
     category: "fun",
