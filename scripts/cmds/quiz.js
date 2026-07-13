95e82d8905acc23448b7a1cbfba4ab7f8ce2e5b1
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:17:54 +0600

    Update quiz.js

--- scripts/cmds/quiz.js
+++ scripts/cmds/quiz.js
@@ -10,7 +10,7 @@ module.exports = {
                 name: "quiz",
                 aliases: ["qz"],
                 version: "1.7",
-                author: "RAHAT",
+                author: "AJMAUL",
                 countDown: 10,
                 role: 0,
                 description: {
