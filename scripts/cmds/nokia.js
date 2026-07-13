2bc28f36081c63018515d0a98fc22274b5d124cc
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:26:28 +0600

    Update nokia.js

--- scripts/cmds/nokia.js
+++ scripts/cmds/nokia.js
@@ -9,7 +9,7 @@ module.exports = {
         config: {
                 name: "nokia",
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 5,
                 role: 0,
                 description: {
