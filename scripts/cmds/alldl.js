de39f6c11879eabab509922f3e23694bfeb585c0
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 14:22:08 +0600

    Update alldl.js

--- scripts/cmds/alldl.js
+++ scripts/cmds/alldl.js
@@ -11,7 +11,7 @@ module.exports = {
                 name: "alldl",
                 aliases: ["download", "dl"],
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 10,
                 role: 0,
                 description: {
