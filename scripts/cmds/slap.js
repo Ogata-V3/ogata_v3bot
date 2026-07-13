fbe308a40aea8dc039519cc926b36ba1b4d25ecf
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:10:26 +0600

    Update slap.js

--- scripts/cmds/slap.js
+++ scripts/cmds/slap.js
@@ -12,7 +12,7 @@ module.exports = {
                 name: "slap",
                 aliases: ["thappor"],
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 10,
                 role: 0,
                 description: {
