472ac850528d89e029278b2807e18d91533939b2
ZORO Vex <mdajmaulislam00@gmail.com>
2026-07-13 13:43:43 +0600

    Update fyp.js

--- scripts/cmds/fyp.js
+++ scripts/cmds/fyp.js
@@ -11,7 +11,7 @@ module.exports = {
         config: {
                 name: "fyp",
                 version: "1.7",
-                author: "RAHAT",
+                author: "Ajmaul",
                 countDown: 5,
                 role: 0,
                 description: {
